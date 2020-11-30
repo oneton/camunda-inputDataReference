7457db0bf4875b1fae82a7ded6b6aed376740559
Anton Tieleman <git@oneton.nl>
2020-11-30 13:01:07 +0100

    Add test to show workaround

--- src/test/java/org/camunda/bpm/unittest/DmnCreateMultipleInputDataTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/DmnCreateMultipleInputDataTestCase.java
@@ -28,51 +28,82 @@ import org.slf4j.LoggerFactory;
 /**
  * @author Anton Tieleman
  */
-public class DmnCreateMultipleInputData {
+public class DmnCreateMultipleInputDataTestCase {
 
-  @Test
-  public void shouldCreateMultipleInputData() {
-	  Logger logger = LoggerFactory.getLogger(DmnCreateMultipleInputData.class);
-	  
-    DmnModelInstance modelInstance = Dmn.createEmptyModel();
+	private static final Logger logger = LoggerFactory.getLogger(DmnCreateMultipleInputDataTestCase.class);
+
+
+	private DmnModelInstance createModel() {
+		DmnModelInstance modelInstance = Dmn.createEmptyModel();
+
+		// Create a basic DMN model with definitions root element
+		Definitions definitions = modelInstance.newInstance(Definitions.class);
+		definitions.setName("InputData error reproduction");
+		definitions.setId("InputDataErrorReproduction");
+		definitions.setNamespace("http://anyschema.com/camunda");
+		modelInstance.setDefinitions(definitions);
+		
+		return modelInstance;
+	}
 	
-    // Create a basic DMN model with definitions root element
-	Definitions definitions = modelInstance.newInstance(Definitions.class);
-	definitions.setName("InputData error reproduction");
-	definitions.setId("InputDataErrorReproduction");
-	definitions.setNamespace("http://anyschema.com/camunda");
-	modelInstance.setDefinitions(definitions);
-	
-	// Add first input data
-	InputData inputData = modelInstance.newInstance(InputData.class);
-	inputData.setId("inputData1");
-	inputData.setName("Input data 1");
-	definitions.addChildElement(inputData);
-	
-	Variable variable = modelInstance.newInstance(Variable.class);
-	variable.setId("inputData1Var");
-	variable.setName("Input data 1");
-	inputData.setInformationItem(variable);
-	
-	// Up to here things still work
-	logger.info("Successfully created and added first inputData");
-	
-	// Try to add second input data
-	modelInstance.newInstance(InputData.class);
-	inputData.setId("inputData2");
-	inputData.setName("Input data 2");
-	
-	logger.info("Successfully created instance of second inputData");
-	
-	// This causes an exception
-	/**
-	 * The message is rather confusing: New child is not a valid child element type: inputData; valid types are: [description, extensionElements, import, itemDefinition, drgElement, artifact, elementCollection, businessContextElement]
-	 * ...because InputData is an instance of DrgElement.
-	 * However: in the conversion from DOM to DMN model, the existing InputData becomes a InputDataReference instance (element names are the same). This is what causes the error.
-	 */
-	definitions.addChildElement(inputData);
-	
-	logger.info("Successfully added second inputData");
-  }
-  
+	private InputData createInputData(DmnModelInstance modelInstance, int index) {
+		String name = String.format("Input data %d", index);
+		
+		InputData inputData = modelInstance.newInstance(InputData.class, String.format("inputData%d", index));
+		inputData.setName(name);
+		
+		Variable variable = modelInstance.newInstance(Variable.class, String.format("inputDataVar%d", index));
+		variable.setName(name);
+		inputData.setInformationItem(variable);
+		
+		return inputData;
+	}
+
+	@Test
+	public void shouldCreateMultipleInputData() {
+		DmnModelInstance modelInstance = createModel();
+		Definitions definitions = modelInstance.getDefinitions();
+
+		// Add first input data
+		InputData inputData = createInputData(modelInstance, 1);
+		definitions.addChildElement(inputData);
+
+		// Create second input data
+		inputData = createInputData(modelInstance, 2);
+
+		logger.info("shouldCreateMultipleInputData: Successfully created instance of second inputData");
+
+		/**
+		 * This causes an exception when called the second time
+		 * 
+		 * The message is rather confusing: New child is not a valid child element type: inputData; valid types are: [description, extensionElements, import, itemDefinition, drgElement, artifact, elementCollection, businessContextElement]
+		 * ...because InputData is an instance of DrgElement.
+		 * However: in the conversion from DOM to DMN model, the existing InputData becomes a InputDataReference instance (element names are the same). This is what causes the error.
+		 */
+		definitions.addChildElement(inputData);
+
+		logger.info("shouldCreateMultipleInputData: Successfully added second inputData");
+	}
+
+	@Test
+	public void shouldCreateMultipleInputDataWorkaround() {
+		DmnModelInstance modelInstance = createModel();
+		Definitions definitions = modelInstance.getDefinitions();
+
+		// Add first input data
+		InputData inputData = createInputData(modelInstance, 1);
+		definitions.addChildElement(inputData);
+
+		// Workaround: the model element instance of the DOM node doesn't need to be created in this case
+		if (inputData.getDomElement().getModelElementInstance() == null) {
+			inputData.getDomElement().setModelElementInstance(inputData);
+		}
+
+		// Create second input data
+		inputData = createInputData(modelInstance, 2);
+
+		definitions.addChildElement(inputData);
+
+		logger.info("shouldCreateMultipleInputDataWorkaround: Successfully added second inputData");
+	}
 }
